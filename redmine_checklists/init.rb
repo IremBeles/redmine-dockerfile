c23117702f592662473fcbdc0b63c5c8f9edfa73
IremBeles <irem.beles@yahoo.com>
2019-05-19 00:55:03 +0300

    issue_done_ratio => true

--- redmine_checklists/init.rb
+++ redmine_checklists/init.rb
@@ -35,7 +35,7 @@ Redmine::Plugin.register :redmine_checklists do
 
   settings :default => {
     :save_log => true,
-    :issue_done_ratio => false
+    :issue_done_ratio => true
   }, :partial => 'settings/checklists/checklists'
 
   Redmine::AccessControl.map do |map|
